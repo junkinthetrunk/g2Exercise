47a22bc95566043c1862ac45a0261812c2c91fc4
junkinthetrunk <maria@Marias-MacBook-Pro.local>
2017-09-18 09:52:03 -0400

    Added views for guide collection cell

--- gExercise/gExercise/GuidesCollectionView.swift
+++ gExercise/gExercise/GuidesCollectionView.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GuidesCollectionView.swift
 //  gExercise
 //
 //  Created by Maria Gomez on 9/18/17.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class GuidesController: UICollectionViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
